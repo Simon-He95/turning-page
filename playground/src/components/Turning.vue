6fa53d70b257c8a87c9bce8f93f8de0569445ad0
Simon He <13917107469@163.com>
2023-03-07 19:14:05 +0800

    chore: init

---
+++ playground/src/components/Turning.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{ images: string[] }>()
+</script>
+
+<template>
+  <ul relative w-70 h-100 list-none ma transform-preserve-3d perspective-900px hover:translate-x-150px transition-1500>
+    <li
+      v-for="src, i in images" :key="src" absolute origin-left rotate-y="[--un-rotate-y]" bg="[url(--un-url)]"
+      :style="{ '--un-url': `url(${src})`, '--un-rotate-y': `${i * 2 - 26}deg`, 'transition': `${2000 - i * 300}ms`, '--un-hover-transition': `${i * 0.6 + 1.4}s` }"
+      w-full h-full bg-contain shadow="[1px_4px_5px_rgba(0,0,0,0.2)]"
+    />
+  </ul>
+</template>
+
+<style scoped>
+ul:hover>li:not(:last-child) {
+  transform: rotateY(-180deg);
+  transition: var(--un-hover-transition) !important;
+}
+</style>
---
+++ playground/src/pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const images = [
+  new URL('../../public/1.jpg', import.meta.url).href,
+  new URL('../../public/2.jpg', import.meta.url).href,
+  new URL('../../public/3.jpg', import.meta.url).href,
+  new URL('../../public/4.jpg', import.meta.url).href,
+  new URL('../../public/5.jpg', import.meta.url).href,
+]
+</script>
+
+<template>
+  <div py4>
+    <Turning :images="images" />
+  </div>
+</template>
+
+<style scoped>
+ul:hover > li:not(:last-child){
+  transform: rotateY(-180deg);
+  transition: var(--un-hover-transition) !important;
+}
+</style>
